c07cd19a95959bb16e0d6c048fa7ef3b967fc9ff
Aryan Nayar <aryannayar47@gmail.com>
2025-06-22 19:36:20 +0530

    Smtp feature for forgot password

--- app/db/base_models.py
+++ app/db/base_models.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 from app.models.chat import Chat, ChatParticipant, Message
 from app.models.pinned_message import PinnedMessage
+from app.models.password_reset import PasswordResetToken
 
 
